cbf5f234df5ff89f2f2ef9c4952dc87cceb89c5c
sweeetpotaooo <sehyun5004@naver.com>
2024-04-03 16:28:54 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div style={{ 
-      display: 'flex', justifyContent: 'center', alignItems: 'center', 
-      width: '100%', height: '100vh'
-      }}>
-      <form style={{ display: 'flex', flexDirection: 'column'}}
-          onSubmit={onSubmitHandler}
-      >
-          <label>Email</label>
-          <input type='email' value={Email} onChange={onEmailHandler}/>
-          <label>Password</label>
-          <input type='password' value={Password} onChange={onPasswordHandler}/>
-          <br />
-          <button formAction=''>
-              Login
-          </button>
-      </form>
-  </div>
-  );
+  
 }
 
 export default App;
