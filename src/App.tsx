7e625e2266359ab6c77fe9f9fe70f10d68157b51
sweeetpotaooo <sehyun5004@naver.com>
2024-04-05 00:49:20 +0900

    buttton

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from './stories/Button'; // Button 컴포넌트를 가져옵니다.
+import { Button } from './stories/Button'; // Button 컴포넌트를 가져오기
 
 export default function App() {
   return (
     <div>
-      <Button label='button'/>
+      <Button label='button'/> 
     </div>
   );
 }
