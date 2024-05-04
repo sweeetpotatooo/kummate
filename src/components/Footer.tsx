372794c597b3ded743be6a4c02070311f3fc4d02
sweeetpotaooo <sehyun5004@naver.com>
2024-05-04 22:55:58 +0900

    footer button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,32 +1,33 @@
 import { FunctionComponent } from 'react';
 import styles from './Footer.module.css';
-import { SiNotion } from "react-icons/si";
-import { GithubOutlined } from "@ant-design/icons";
-import { Button} from "antd"
+import { SiNotion } from 'react-icons/si';
+import { GithubOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 
-const Footer:FunctionComponent = () => {
-  	return (
-    		<div className={styles.footer}>
-      			<div className={styles.footerChild} />
-      			<div className={styles.kummateParent}>
-        				<b className={styles.kummate}>KUMMATE</b>
-        				<b className={styles.copyright2024}>{`Copyright ⓒ 2024 by Kummate `}</b>
-      			<Button
-						shape="circle"
-						icon={
-							<GithubOutlined
-								style={{ fontSize: "20px", color: "#6231ef" }}/>
-						}
-						/>
-						<Button
-						shape="circle"
-						icon={
-							<SiNotion
-								style={{ fontSize: "20px", color: "#6231ef" }}/>
-						}
-						/>
-						</div>
-    		</div>);
+const Footer: FunctionComponent = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.footerChild} />
+      <div className={styles.kummateParent}>
+        <b className={styles.kummate}>KUMMATE</b>
+        <b className={styles.copyright2024}>{`Copyright ⓒ 2024 by Kummate`}</b>
+				</div>
+        <div className={styles.socialButtons}>
+            <Button
+              shape="circle"
+              icon={<GithubOutlined style={{ fontSize: '20px', color: '#38773e' }} />}
+              aria-label="GitHub"
+              className={styles.socialButton}
+            />
+            <Button
+              shape="circle"
+              icon={<SiNotion style={{ fontSize: '20px', color: '#38773e' }} />}
+              aria-label="Notion"
+              className={styles.socialButton}
+            />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
