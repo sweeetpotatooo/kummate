f14265b1a29bf2ee15887b2d652b82efa61e159a
sweeetpotaooo <sehyun5004@naver.com>
2024-05-07 00:26:45 +0900

    mainpage0507

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -10,6 +10,7 @@ const Body = () => {
         <>
         <div className={styles.bodyContainer1}>
           <h2 className={styles.title}> 룸메이트 구해요</h2>
+          <div className={styles.groupAndArrowContainer}>
           <div className={styles.groupContainer}>
               <Group />
               <Group />
@@ -18,18 +19,21 @@ const Body = () => {
             </div>
             <img className={styles.polygonIcon1} alt="" src={arrow_left} />
             <img className={styles.polygonIcon2} alt="" src={arrow_right} />
+            </div>
           </div>
       
         <div className={styles.bodyContainer2}>
           <h2 className={styles.title}> KUMMATE에서 추천하는 룸메이트를 만나보세요</h2>
+          <div className={styles.groupAndArrowContainer}>
           <div className={styles.groupContainer}>
             <RecommendationCard/> 
             <RecommendationCard/> 
             <RecommendationCard/> 
             <RecommendationCard/> 
-          </div>
+            </div>
             <img className={styles.polygonIcon1} alt="" src={arrow_left} />
             <img className={styles.polygonIcon2} alt="" src={arrow_right} />
+          </div>
         </div>
         </>
 
