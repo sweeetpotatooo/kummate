3ce5498f74c5763f801cc0a6ce86bd7fe0c738db
sweeetpotaooo <sehyun5004@naver.com>
2024-05-06 23:47:39 +0900

    main button

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Group from './Group';
 import RecommendationCard from './RecommendCard'
 import styles from './Body.module.css';
+import arrow_left from '../assets/Polygon 1.svg'
+import arrow_right from '../assets/Polygon 2.svg'
 
 const Body = () => {
     return (
@@ -9,12 +11,14 @@ const Body = () => {
         <div className={styles.bodyContainer1}>
           <h2 className={styles.title}> 룸메이트 구해요</h2>
           <div className={styles.groupContainer}>
-            <Group />
-            <Group />
-            <Group />
-            <Group />
+              <Group />
+              <Group />
+              <Group />
+              <Group />
+            </div>
+            <img className={styles.polygonIcon1} alt="" src={arrow_left} />
+            <img className={styles.polygonIcon2} alt="" src={arrow_right} />
           </div>
-        </div>
       
         <div className={styles.bodyContainer2}>
           <h2 className={styles.title}> KUMMATE에서 추천하는 룸메이트를 만나보세요</h2>
@@ -24,6 +28,8 @@ const Body = () => {
             <RecommendationCard/> 
             <RecommendationCard/> 
           </div>
+            <img className={styles.polygonIcon1} alt="" src={arrow_left} />
+            <img className={styles.polygonIcon2} alt="" src={arrow_right} />
         </div>
         </>
 
